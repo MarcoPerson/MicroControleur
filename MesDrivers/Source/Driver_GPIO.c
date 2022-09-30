37762e271dd60b270593a63513b79c73278f44c4
Komlan-Epe-Nsin-Merveilles Agbeti-Messan <agbeti-m@insa-toulouse.fr>
2022-09-30 13:11:14 +0200

    GPIO Corrigé avec les Tests valides

--- MesDrivers/Source/Driver_GPIO.c
+++ MesDrivers/Source/Driver_GPIO.c
@@ -22,13 +22,13 @@ void MyGPIO_Init ( MyGPIO_Struct_TypeDef * GPIOStructPtr ){
 		GPIOStructPtr->GPIO->CRL |= GPIOStructPtr->GPIO_Conf << GPIOStructPtr->GPIO_Pin*4;
 	}
 	else{
-		GPIOStructPtr->GPIO->CRL &= ~(0xF << (GPIOStructPtr->GPIO_Pin%8)*4);
+		GPIOStructPtr->GPIO->CRH &= ~(0xF << (GPIOStructPtr->GPIO_Pin%8)*4);
 		GPIOStructPtr->GPIO->CRH |= GPIOStructPtr->GPIO_Conf << (GPIOStructPtr->GPIO_Pin%8)*4;
 	}
 }
 
 int MyGPIO_Read ( GPIO_TypeDef * GPIO , char GPIO_Pin ){
-	if((GPIO->IDR & (uint16_t)GPIO_Pin) == 0){
+	if((GPIO->IDR & (0x01<<GPIO_Pin)) == 0){
 		return 0;
 	}
 	else{
@@ -38,10 +38,10 @@ int MyGPIO_Read ( GPIO_TypeDef * GPIO , char GPIO_Pin ){
 
 
 void MyGPIO_Set ( GPIO_TypeDef * GPIO , char GPIO_Pin ){
-	GPIO->ODR |= (uint16_t)GPIO_Pin;
+	GPIO->ODR |= 0x01<<(GPIO_Pin);
 }
 void MyGPIO_Reset ( GPIO_TypeDef * GPIO , char GPIO_Pin ){
-	GPIO->ODR &= ~((uint16_t)GPIO_Pin);
+	GPIO->ODR &= ~(0x01<<(GPIO_Pin));
 }
 void MyGPIO_Toggle ( GPIO_TypeDef * GPIO , char GPIO_Pin ){
 	if((GPIO->ODR & (uint16_t)GPIO_Pin) == 0){
--- Pjet_DevGPIO/Source/Principal.c
+++ Pjet_DevGPIO/Source/Principal.c
@@ -3,19 +3,35 @@
 
 
 int main(void){
-	//GPIOC->CRH |= ((0x01 << 9) | (0x01 << 2));
-	//GPIOC->CRH &= ~((0x01 << 11) | (0x01 << 10) | (0x01 << 8) | (0x01 << 3) |(0x01 << 1) | (0x01 << 0));
+	/*GPIOC->CRH |= ((0x01 << 9) | (0x01 << 2));
+	GPIOC->CRH &= ~((0x01 << 11) | (0x01 << 10) | (0x01 << 8) | (0x01 << 3) |(0x01 << 1) | (0x01 << 0));
 	
 	//Open Drain
 	GPIOC->CRH |= ((0x01 << 10) | (0x01 << 9) | (0x01 << 2));
 	GPIOC->CRH &= ~((0x01 << 11) | (0x01 << 8) | (0x01 << 3) |(0x01 << 1) | (0x01 << 0));
+	*/
+	//int state=0;
+	MyGPIO_Struct_TypeDef PC8, PC10;
+	PC8.GPIO = GPIOC;
+	PC8.GPIO_Pin = 8;
+	PC8.GPIO_Conf = In_Floating;
+	
+	PC10.GPIO = GPIOC;
+	PC10.GPIO_Pin = 10;
+  PC10.GPIO_Conf = Out_OD;
+	
+	MyGPIO_Init(&PC8);
+	MyGPIO_Init(&PC10);
+	
+	
+	
 	
 	while(1){
-		if ((GPIOC->IDR & GPIO_IDR_IDR8) != 0){
-			GPIOC->ODR &= ~(GPIO_ODR_ODR10);
+		if (MyGPIO_Read(PC8.GPIO, PC8.GPIO_Pin) != 0){
+			MyGPIO_Reset(PC10.GPIO, PC10.GPIO_Pin);
 		}
 		else{
-			GPIOC->ODR |= GPIO_ODR_ODR10;
+			MyGPIO_Set(PC10.GPIO, PC10.GPIO_Pin);
 		}
 	}
 }
